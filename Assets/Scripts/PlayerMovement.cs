92a645b4751eb946825b6c4b80aaa0a0a5202261
unknown <Crosby.Nelson@iam.local>
2017-09-18 15:18:56 -0500

    Followed along with in class demo

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        //Debug.Log("Hello. This code is inside of Start.");
+
+        //this code teleports my game object to a new position
+        //transform.position = new Vector3(0, 0, 0);
+
+        
+
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //Debug.Log("This is called from Update.");
+
+        transform.Translate(0.1f, 0, 0);
+
+    }
+}
